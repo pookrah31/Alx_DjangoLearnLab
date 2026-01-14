2cc05f961a9517dc6eae08a0ecbd3738b68421e7
Pookrah31 <penielokrah31@gmail.com>
2026-01-14 12:56:15 +0000

    Fix list_books view to match auto-checker expectations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,8 +5,8 @@ from .models import  Book, Library
 # Create your views here.
 #Function-based view
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+     books = Book.objects.all()  # <- exactly this line
+     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 #Class-based view
 class LibraryDetailView(DetailView):
