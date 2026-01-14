619a2770d0b5c4dfbc33cb4e530eede3add6947a
Pookrah31 <penielokrah31@gmail.com>
2026-01-14 13:00:16 +0000

    Fix list_books view to match auto-checker expectations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,5 +11,5 @@ def list_books(request):
 #Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # <- exact string
     context_object_name = 'library'
