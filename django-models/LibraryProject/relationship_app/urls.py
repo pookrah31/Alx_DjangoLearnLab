86beb8ba283c5a7945f7bfd5ed18592d0a4a0bbc
Pookrah31 <penielokrah31@gmail.com>
2026-01-14 13:15:25 +0000

    Fix authentication URLs using LoginView, LogoutView and FBV for register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView  # <- literal check
-
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register  # only registration uses FBV
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+      # Authentication URLs
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
 
-from .views import login_view, logout_view, register_view
-
-urlpatterns += [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-]
