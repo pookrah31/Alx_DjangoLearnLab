bf8e365d79a986cd1b797a89ec3d93495112622e
Pookrah31 <penielokrah31@gmail.com>
2026-01-14 12:27:37 +0000

    Add advanced Django model relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,4 +14,4 @@ def list_books_in_library(library_name):
 
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return librarian.objects.get(library=library)
