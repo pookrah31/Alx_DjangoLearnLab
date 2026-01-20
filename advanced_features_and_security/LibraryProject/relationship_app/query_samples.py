a9b72fd55d7d1acf37848f40e1aededcf5881491
Pookrah31 <penielokrah31@gmail.com>
2026-01-20 12:58:23 +0000

    Initial commit - custom user model setup

---
+++ advanced_features_and_security/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
+
+
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,24 @@
+# relationship_app/urls.py
+from django.urls import path
+from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views  # use views.register
+
+urlpatterns = [
+    # Existing views
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication URLs
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
+
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
+]
