ac66a5ecfdd90ff45e9f65ac3107c15045af5d17
Pookrah31 <penielokrah31@gmail.com>
2026-01-28 07:46:00 +0000

    Implementing Authentication and Permissions in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
@@ -9,7 +10,18 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+# This ViewSet uses TokenAuthentication with IsAuthenticated permissions
+# Only users with valid tokens can perform CRUD operations
+
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+           return [IsAdminUser()]
+           return [IsAuthenticatedorReadOnly()]   
+        
+            
