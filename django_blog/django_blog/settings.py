019b9a7557ad09f3a575526a7752b9798c7e9f68
Pookrah31 <penielokrah31@gmail.com>
2026-02-10 01:16:27 +0000

    Fix: Changed database from sqlite to postgresql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Peniel@31',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
