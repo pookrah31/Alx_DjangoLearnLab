7b73d46b733590962b765b182c5d488b4c3b1e40
Pookrah31 <penielokrah31@gmail.com>
2026-02-10 23:52:09 +0000

    --amend

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/',views.profile_view, name='profile'),
-     path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/new/", PostCreateView.as_view(), name="post-create"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
+    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
