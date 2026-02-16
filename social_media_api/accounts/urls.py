c54843cb8fda32c35a951ff15e40ed695ecaaf99
Pookrah31 <penielokrah31@gmail.com>
2026-02-16 09:50:38 +0000

    --amend

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('follow/<str:username>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<str:username>/', UnfollowUserView.as_view(), name='unfollow-user'),
+       path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
