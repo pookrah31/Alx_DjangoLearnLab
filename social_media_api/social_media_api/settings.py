5f04ecaac33be7b1b22ec4e0cb0f4340e3899536
Pookrah31 <penielokrah31@gmail.com>
2026-02-17 15:54:22 +0000

    Explicitly define PORT for ALX checker

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -90,6 +90,11 @@ DATABASES = {
     )
 }
 
+DATABASES['default']['PORT'] = os.environ.get('DB_PORT', '5432')
+DATABASES['default']['HOST'] = os.environ.get('DB_HOST', '')
+DATABASES['default']['USER'] = os.environ.get('DB_USER', '')
+DATABASES['default']['PASSWORD'] = os.environ.get('DB_PASSWORD', '')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
