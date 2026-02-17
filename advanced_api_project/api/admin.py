4c41debe8142c673a3aec47c63c7efc9cb013ae7
Pookrah31 <penielokrah31@gmail.com>
2026-02-17 12:49:26 +0000

    Fix Heroku buildpack detection

--- advanced_api_project/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Author, Book
-
-# Register Author and Book models to access them in the admin
-admin.site.register(Author)
-admin.site.register(Book)
