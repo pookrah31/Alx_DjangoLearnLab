ae7d54f5624ac0a1ec4b8d30af2893e5589260f6
Pookrah31 <penielokrah31@gmail.com>
2026-02-17 13:46:08 +0000

    Fix ALLOWED_HOSTS using environment variable

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -29,13 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    'social-meadia-api-peniel.herokuapp.com',
-    'https://social-media-api-peniel-1a6fbf772928.herokuapp.com/'
-
-]
+# This stays inside settings.py
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 
 # Application definition
